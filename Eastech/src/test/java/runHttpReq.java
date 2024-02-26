b44a5c309fac1bbdaa6160969089499fa4ae3a5c
rasha-bazbaz <rasha.bazbaz@gmail.com>
2024-02-26 18:43:40 +0200

    Post req

--- Eastech/src/test/java/runHttpReq.java
+++ Eastech/src/test/java/runHttpReq.java
@@ -7,5 +7,11 @@ public class runHttpReq {
 		System.out.println(resp.body()); 
 		System.out.println("=-=-=-=-=-=-=-=-=-==-=--=");
 		System.out.println(resp.statusCode());
+		
+		String body ="{\"userId\": 1,\"title\": \"test title\",\"body\": \"test body\"}";
+		HttpResponse<String> resp2 = RequestsCall.PostRequest("https://jsonplaceholder.typicode.com/posts", body);
+		System.out.println(resp2.body()); 
+		System.out.println("=-=-=-=-=-=-=-=-=-==-=--=");
+		System.out.println(resp2.statusCode());
 	}
 }
