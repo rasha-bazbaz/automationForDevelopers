e6627f8c6932500f0f1d3f06d3d50484d8619c2f
rasha-bazbaz <rasha.bazbaz@gmail.com>
2024-02-29 17:21:37 +0200

    work with csv

--- Eastech/src/main/java/WorkWithCSV.java
+++ Eastech/src/main/java/WorkWithCSV.java
@@ -1,4 +1,6 @@
+import java.awt.image.RescaleOp;
 import java.io.FileReader;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 import com.opencsv.CSVReader;
@@ -14,11 +16,46 @@ public class WorkWithCSV {
 		reader.readNext();
 		while ((record = reader.readNext()) != null) {
 			data.add(record);
-			
+
 		}		
 		reader.close();
 		return data;
 	}
-	
-	
+
+	public static void main(String[] args) throws Exception {
+		ArrayList<String[]> data = WorkWithCSV.readAllLines("TestCasesFull.csv");
+		for(int i=0;i<data.size();i++) {
+			String type = data.get(i)[0];
+			String url = "https://jsonplaceholder.typicode.com" + data.get(i)[1];
+			String body = data.get(i)[2];
+			int expStatus = Integer.parseInt(data.get(i)[3]);
+			switch (type) {
+			case "GET": {
+				HttpResponse resp = RequestsCall.GetRequest(url);
+				if (resp.statusCode() == expStatus) {
+					System.out.println("pass");
+				}else {
+					System.out.println("fail");
+				}
+				break;
+			}
+			case "POST": {
+
+				break;
+			}
+			case "PUT": {
+
+				break;
+			}
+			case "DELETE": {
+
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + type);
+			}
+		}
+
+	}
+
 }
